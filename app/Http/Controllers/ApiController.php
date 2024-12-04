1d04be1e2464299108802f4c89a4ac492f2609b3
subhomoy samanta <subhomoy@keylines.net>
2024-12-04 14:02:20 +0530

    authentication api implemented

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -984,8 +984,9 @@ class ApiController extends Controller
                         $expiry     = date('d/m/Y H:i:s', $getTokenValue['data'][4]);
                         $getUser    = Employees::where('id', '=', $uId)->first();
                         if($getUser){
+                            $getEmployeeType     = EmployeeType::select('name')->where('id', '=', $getUser->employee_type_id)->first();
                             $fields = [
-                                'user_type'                 => 'teacher',
+                                'user_type'                 => (($getEmployeeType)?$getEmployeeType->name:''),
                                 'entity_name'               => $getUser->name,
                                 'email'                     => $getUser->email,
                                 'is_email_verify'           => 1,
