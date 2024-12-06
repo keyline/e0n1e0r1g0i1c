ffc61e472be63fc3a651150dc28e3db1835003ac
subhomoy samanta <subhomoy@keylines.net>
2024-12-06 15:37:43 +0530

    api updated

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1442,6 +1442,7 @@ class ApiController extends Controller
                                         $apiExtraData               = http_response_code();
                                     } else {
                                         $order_image        = $order_image;
+                                        echo count($order_image);
                                         Helper::pr($order_image,0);die;
                                         $orderImages        = [];
                                         if(!empty($order_image)){
