c82ef94ca83757652ddfb676cedf67e6633f50cc
subhomoy samanta <subhomoy@keylines.net>
2025-01-02 14:55:23 +0530

    api updated

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -280,42 +280,46 @@ class Helper{
     public static function getDateListDescending($month, $year) {
       $startDate = new DateTime("$year-$month-01");
       if($month==1) {
-          $last_date = 31;
+        $last_date = 31;
       }
       if($month==2) {
-          $last_date = 28;
+        $last_date = 28;
       }
       if($month==3) {
-          $last_date = 31;
+        $last_date = 31;
       }
       if($month==4) {
-          $last_date = 30;
+        $last_date = 30;
       }
       if($month==5) {
-          $last_date = 31;
+        $last_date = 31;
       }
       if($month==6) {
-          $last_date = 30;
+        $last_date = 30;
       }
       if($month==7) {
-          $last_date = 31;
+        $last_date = 31;
       }
       if($month==8) {
-          $last_date = 31;
+        $last_date = 31;
       }
       if($month==9) {
-          $last_date = 30;
+        $last_date = 30;
       }
       if($month==10) {
-          $last_date = 31;
+        $last_date = 31;
       }
       if($month==11) {
-          $last_date = 30;
+        $last_date = 30;
       }
       if($month==12) {
-          $last_date = 31;
+        $last_date = 31;
       }
-      $endDate = new DateTime("$year-$month-$last_date"); // Current date
+      if(date('Y') == $year && date('m') == $month){
+        $endDate = new DateTime();
+      } else {
+        $endDate = new DateTime("$year-$month-$last_date");
+      }
       // $endDate = new DateTime(); // Current date
       $dates = [];
 
