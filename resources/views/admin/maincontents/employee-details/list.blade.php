e81562428c30327700b39ea46343d315e31ffa01
subhomoy samanta <subhomoy@keylines.net>
2025-01-02 12:41:06 +0530

    admin panel updated

--- resources/views/admin/maincontents/employee-details/list.blade.php
+++ resources/views/admin/maincontents/employee-details/list.blade.php
@@ -76,7 +76,7 @@ $controllerRoute = $module['controller_route'];
                                                         ->where('employees.id', '=', $parent_id[$d])
                                                         ->first();
                         ?>
-                          <li><?=(($getEmployee)?$getEmployee->name . '('.$getEmployee->employee_type_prefix.')':'')?></li>
+                          <li><?=(($getEmployee)?$getEmployee->employee_name . '('.$getEmployee->employee_type_prefix.')':'')?></li>
                         <?php } }?>
                       </ul>
                     </td>
