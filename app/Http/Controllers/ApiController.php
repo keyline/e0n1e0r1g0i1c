285f7d86ce3ed8b4eb801c5c0ca30fe95bd99ba7
subhomoy samanta <subhomoy@keylines.net>
2025-01-03 16:52:11 +0530

    admin panel & api updated

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3390,7 +3390,7 @@ class ApiController extends Controller
                                             }
                                         }
                                         $emp_type_string    = implode(',', $empTypeIds);
-                                        $getEmps            = Employees::select('id', 'employee_no', 'name', 'email', 'phone', 'short_bio', 'address', 'profile_image')->whereIn('employee_type_id', [$emp_type_string])->where('status', '=', 1)->whereJsonContains('assign_district', $districtIds[$d])->orderBy('name', 'ASC')->get();
+                                        $getEmps            = Employees::select('id', 'employee_no', 'name', 'email', 'phone', 'short_bio', 'address', 'profile_image')->whereIn('employee_type_id', $emp_type_string)->where('status', '=', 1)->whereJsonContains('assign_district', $districtIds[$d])->orderBy('name', 'ASC')->get();
                                     }
                                     
                                     if($getEmps){
