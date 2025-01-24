9081192a9834831a5570f4ff4b5ad1582eda87e9
subhomoy samanta <subhomoy@keylines.net>
2025-01-24 23:49:37 +0530

    update attendance api updated

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2855,7 +2855,7 @@ class ApiController extends Controller
                         $type                   = $requestData['type'];
                         $latitude               = $requestData['latitude'];
                         $longitude              = $requestData['longitude'];
-                        Helper::pr($requestData);
+                        // Helper::pr($requestData);
                         $address                = $this->geolocationaddress($latitude, $longitude);
                         /* trip start */
                             if($type == 'IN'){
